4367025aa2896c92e5d7988d3cce85bb10877e0a
Thomas Scattergood <tscattergood@softwareinstitute.com>
2021-10-30 10:43:08 +0100

    changed test package

--- src/test/java/com/softwareinstitute/training/scattergood/thomas/movieProjectMdemo/movieProjectMdemoApplicationTests.java
+++ src/test/java/com/softwareinstitute/training/scattergood/thomas/movieProjectMdemo/movieProjectMdemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.movieProjectMdemo;
+package com.softwareinstitute.training.scattergood.thomas.movieProjectMdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
