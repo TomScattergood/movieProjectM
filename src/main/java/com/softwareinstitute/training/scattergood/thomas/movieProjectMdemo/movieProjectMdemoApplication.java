3620b4ff1a41fca9839b05a5d6274b0442263585
Thomas Scattergood <tscattergood@softwareinstitute.com>
2021-11-26 09:41:15 +0000

    removed jenkins comment

--- src/main/java/com/softwareinstitute/training/scattergood/thomas/movieProjectMdemo/movieProjectMdemoApplication.java
+++ src/main/java/com/softwareinstitute/training/scattergood/thomas/movieProjectMdemo/movieProjectMdemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Optional;
-// jenkins comment test
+//
 @SpringBootApplication
 @RestController
 @RequestMapping({"/customers", "/countries", "/actors"})
